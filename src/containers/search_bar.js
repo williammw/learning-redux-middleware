002af82bdb394aa105a89935d6c8f4da0daf9b88
WONG <wmwwong@legco.hksar>
2017-10-03 16:42:37 +0800

    docs

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,7 +13,7 @@ constructor(props){
     this.onFormSubmit = this.onFormSubmit.bind(this);
 }
 onInputChange(event){
-    console.log(event.target.value)
+//    console.log(event.target.value)
     this.setState({term :event.target.value})
 }
 onFormSubmit(event){
